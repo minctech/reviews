04ea448a729b6a3a418635e912257f94052f1846
Anthony Fojas <anthonyjfojas@gmail.com>
2019-08-23 15:56:58 -0700

    Implement Express server

---
+++ client/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Reviews extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // this.state = {
+
+  // };
+
+  render() {
+    return (
+      <div>hello world</div>
+    )
+  }
+}
+
+ReactDOM.render(<Reviews />, document.getElementById('reviews'));
