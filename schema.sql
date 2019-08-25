5d1dca3b82c9b8a135fcd2046e4255d0bc1fd299
Anthony Fojas <anthonyjfojas@gmail.com>
2019-08-25 15:27:51 -0700

    Create mysql schema and populate database with fake data

---
+++ schema.sql
@@ -0,0 +1,51 @@
+DROP DATABASE IF EXISTS reviewsmodule;
+
+CREATE DATABASE reviewsmodule;
+
+USE reviewsmodule;
+
+CREATE TABLE listings (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  host_pic VARCHAR(100) NOT NULL,
+  host_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE users (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  pic VARCHAR(100) NOT NULL,
+  name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE responses (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  comment TEXT NOT NULL
+);
+
+CREATE TABLE reviews (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  listings_id INT NOT NULL,
+  users_id INT NOT NULL,
+  date VARCHAR(50),
+  comment TEXT NOT NULL,
+  accuracy INT NOT NULL,
+  communication INT NOT NULL,
+  cleanliness INT NOT NULL,
+  location INT NOT NULL,
+  checkin INT NOT NULL,
+  value INT NOT NULL,
+  responses_id INT NULL,
+  FOREIGN KEY (listings_id) REFERENCES listings (id),
+  FOREIGN KEY (users_id) REFERENCES users (id),
+  FOREIGN KEY (responses_id) REFERENCES responses (id)
+)
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u <USER> -p < schema.sql
+
+  RESPONSE INSERT ERROR:
+    Error: ER_NO_REFERENCED_ROW_2: Cannot add or update a child row: a foreign key constraint fails (`reviewsmodule`.`reviews`, CONSTRAINT `reviews_ibfk_3` FOREIGN KEY (`responses_id`) REFERENCES `responses` (`id`))
+      at Query.Sequence._packetToError (/mnt/c/Users/ANTHONY/hrsf122/reviews/node_modules/mysql/lib/protocol/sequences/Sequence.js:47:14)
+
+    sqlMessage: 'Cannot add or update a child row: a foreign key constraint fails (`reviewsmodule`.`reviews`, CONSTRAINT `reviews_ibfk_3` FOREIGN KEY (`responses_id`) REFERENCES `responses` (`id`))',
+*/
