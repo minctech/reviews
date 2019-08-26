2535289d062ca34a24489d7b8602c376db22591f
Anthony Fojas <anthonyjfojas@gmail.com>
2019-08-26 14:40:08 -0700

    Set up initial states

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Reviews extends React.Component {
+class ReviewsModule extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      reviews: [], //arr of all reviews for a particular listing, can use length to show total # reviews
+      search: '', //used to filter reviews
+      accuracy: 0,      //average of all accuracy ratings
+      communication: 0, //average of all communication ratings
+      cleanliness: 0,   //average of all cleanliness ratings
+      location: 0,      //average of all location ratings
+      checkin: 0,       //average of all checkin ratings
+      value: 0,         //average of all value ratings
+      overall: 0 //average of all the averages
+    };
   }
 
-  // this.state = {
-  // };
+
 
   render() {
     return (
-      <div>hello world</div>
+      <div>Hello, so we meet again, World</div>
     )
   }
 }
 
-ReactDOM.render(<Reviews />, document.getElementById('reviews'));
+ReactDOM.render(<ReviewsModule />, document.getElementById('reviews'));
