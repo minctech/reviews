4f1a9564384144ce1aa093219fd4387348d9df29
Anthony Fojas <anthonyjfojas@gmail.com>
2019-09-02 00:07:51 -0700

    Styled base component

--- client/src/components/ReviewEntry.jsx
+++ client/src/components/ReviewEntry.jsx
@@ -1,5 +1,50 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Block = styled.div`
+  padding: 25px 0 0 0;
+  border-bottom: 1px solid #eee;
+`;
+
+const ResponseBlock = styled.div`
+  display: flex;
+  padding: 25px 0 0 25px;
+`;
+
+const ProfilePic = styled.img`
+  width: 50px;
+  border-radius: 50%;
+`;
+
+const ResponseProfilePic = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const NameDate = styled.span`
+  display: inline-block;
+  vertical-align: top;
+  padding: 5px 0 25px 20px;
+`;
+
+const NameResponseDate = styled.span`
+  display: flex;
+  flex-direction: column;
+  vertical-align: top;
+  padding: 5px 0 25px 20px;
+`;
+
+const Name = styled.span`
+  font-weight: bold;
+  padding-bottom: 5px;
+`;
+
+const ResponseDate = styled.div`
+  padding: 10px 0 0 0;
+  color: #888;
+`;
 
 class ReviewEntry extends React.Component {
   constructor(props) {
@@ -43,29 +88,34 @@ class ReviewEntry extends React.Component {
     let response = '';
     if (this.props.reviewEntry.responses_id) {
       response =
-        <div>
-          <div>
-            <img src={this.props.hostInfo.host_pic} alt=""/>
-          </div>
-          <div>
-            Response from {this.props.hostInfo.host_name}
-          </div>
-          <div>
-            {this.state.response}
-          </div>
-        </div>
+        <ResponseBlock>
+          <ResponseProfilePic src={this.props.hostInfo.host_pic} alt=""/>
+          <NameResponseDate>
+            <Name>
+              Response from {this.props.hostInfo.host_name}:
+            </Name>
+            <div>
+              {this.state.response}
+            </div>
+            <ResponseDate>
+              {this.props.reviewEntry.date}
+            </ResponseDate>
+          </NameResponseDate>
+        </ResponseBlock>
     }
 
     return(
-      <div>
+      <Block>
         <div>
-          <img src={this.state.user.pic} alt=""/>
-        </div>
-        <div>
-          {this.state.user.name}
-        </div>
-        <div>
-          {this.props.reviewEntry.date}
+          <ProfilePic src={this.state.user.pic} alt=""/>
+          <NameDate>
+            <Name>
+              {this.state.user.name}
+            </Name>
+            <div>
+              {this.props.reviewEntry.date}
+            </div>
+          </NameDate>
         </div>
         <div>
           {this.props.reviewEntry.comment}
@@ -74,11 +124,7 @@ class ReviewEntry extends React.Component {
           {response}
         </div>
         <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-      </div>
+      </Block>
     )
   }
 }
