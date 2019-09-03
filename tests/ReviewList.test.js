2e0dafd3b1fc43f7f67b81d3f84f041d0645175c
Anthony Fojas <anthonyjfojas@gmail.com>
2019-09-03 10:43:12 -0700

    Update tests

---
+++ tests/ReviewList.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import ReviewList from '../client/src/components/ReviewList';
+
+describe('ReviewList component', () => {
+  const states = {
+    reviews: [{}, {}],
+    filteredReviews: [{}],
+    search: '',
+    accuracy: 5,
+    communication: 4,
+    cleanliness: 3,
+    location: 4,
+    checkin: 5,
+    value: 3,
+    overall: 4
+  };
+
+  it('renders successfully', () => {
+    shallow(<ReviewList allStates={states} />);
+  });
+
+  it('displays filtered searches', () => {
+    const wrapper = shallow(<ReviewList allStates={states} />);
+    expect()
+  })
+});
